223ceba9a7d1932ac0dc10e7d09f523490bc5c4d
alim1381 <ali.moaydi2020@gmail.com>
2023-11-18 00:31:21 +0330

    message pagination

--- app/(privete routes)/chat/page.js
+++ app/(privete routes)/chat/page.js
@@ -16,22 +16,36 @@ function Chat() {
   // state
   const messagesState = useSelector((state) => state.messagesState);
   const [pageNumber, setPageNumber] = useState(1);
+  const [newMessage, setNewMessage] = useState(0);
+  const [lastMessage, setLastMessage] = useState(null);
+  const [isFinishedMessage, setIsFinishedMessage] = useState(false);
+
   const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(null);
 
   // api
   const [previousMsgRes, previousMsgErr, previousMsgLoading, getPreviousMsg] =
-    useRequests({ method: "GET", path: `/message?page=${pageNumber}` });
+    useRequests({
+      method: "GET",
+      path: `/message?page=${pageNumber}&newMessage=${newMessage}`,
+    });
 
   // get previous Messages
   useEffect(() => {
-    getPreviousMsg();
-  }, []);
+    if (!isFinishedMessage) {
+      setLoading(true)
+      getPreviousMsg();
+    }
+  }, [pageNumber]);
 
   // add Response to Redux State
   useEffect(() => {
     if (previousMsgRes) {
       dispatch(addToBackMessages(previousMsgRes));
+      setLoading(false)
+    }
+    if (previousMsgRes !== null && previousMsgRes.length === 0) {
+      setIsFinishedMessage(true);
     }
   }, [previousMsgRes]);
 
@@ -39,6 +53,24 @@ function Chat() {
   const scrollToEnd = () => {
     endMessage.current?.scrollIntoView({ behavior: "smooth" });
   };
+
+  // fetching by scroll
+  const observerRef = new IntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+      setPageNumber((prev) => prev + 1);
+    }
+  });
+
+  useEffect(() => {
+    if (lastMessage) {
+      observerRef.observe(lastMessage);
+    }
+    return () => {
+      if (lastMessage) {
+        observerRef.unobserve(lastMessage);
+      }
+    };
+  }, [lastMessage]);
 
   return (
     <div className=" text-white relative h-full">
@@ -52,7 +84,7 @@ function Chat() {
       <div className=" h-[75vh] sc px-5 overflow-y-scroll flex flex-col-reverse">
         <div ref={endMessage} />
         {messagesState.map((msg, index) => (
-          <div key={index}>
+          <div key={index} ref={setLastMessage}>
             <Message {...msg} />
           </div>
         ))}
